2b29d22de95ddc25c192992db8d46a8d4aba43e0
midnight9040 <midnight9040@gmail.com>
2019-11-25 09:00:09 +0900

    //create .java file for creating a package 20191125

---
+++ src/main/java/com/jamong/domain/MemberVO.java
@@ -0,0 +1,6 @@
+package com.jamong.domain;
+
+/**임시 .java파일 (패키지 생성을 위해서)**/
+public class MemberVO {
+
+}
---
+++ src/main/java/com/jamong/service/IndexService.java
@@ -0,0 +1,6 @@
+package com.jamong.service;
+
+/**임시 .java파일 (패키지 생성을 위해서)**/
+public interface IndexService {
+
+}
