4a97b6f49424312020cbae67747ecdd13e4abb5b
gkrdud0822 <hakyoung0822@gmail.com>
2019-11-26 14:53:17 +0900

    주석추가

--- src/main/webapp/resources/SQL/Jamong_author.sql
+++ src/main/webapp/resources/SQL/Jamong_author.sql
@@ -1,16 +1,16 @@
 -- 작가신청 테이블
 CREATE TABLE author(
-aut_no NUMBER(38) PRIMARY KEY,
-aut_intro VARCHAR2(4000) NOT NULL,
-aut_plan VARCHAR2(4000) NOT NULL,
-aut_url1 VARCHAR2(200),
+aut_no NUMBER(38) PRIMARY KEY,     -- 작가신청 번호(seq)
+aut_intro VARCHAR2(4000) NOT NULL, -- 작가 소개
+aut_plan VARCHAR2(4000) NOT NULL,  -- 작가활동계획
+aut_url1 VARCHAR2(200),			   -- 제출 URL
 aut_url2 VARCHAR2(200),
 aut_url3 VARCHAR2(200),
-aut_file1 VARCHAR2(200),
+aut_file1 VARCHAR2(200),		   -- 제출 파일
 aut_file2 VARCHAR2(200),
 aut_file3 VARCHAR2(200),
-aut_date DATE,
-mem_no NUMBER(38)
+aut_date DATE, 					   -- 신청일
+mem_no NUMBER(38)				   -- 회원번호 참조컬럼
 );
 
 -- 시퀀스 생성
--- src/main/webapp/resources/SQL/Jamong_inquire.sql
+++ src/main/webapp/resources/SQL/Jamong_inquire.sql
@@ -1,17 +1,17 @@
 -- 문의하기 테이블 구성
 CREATE TABLE inquire(
-inq_no NUMBER(38) PRIMARY KEY,
-inq_item1 VARCHAR2(100) NOT NULL,
+inq_no NUMBER(38) PRIMARY KEY,    -- 문의번호(seq)
+inq_item1 VARCHAR2(100) NOT NULL, -- 문의 항목
 inq_item2 VARCHAR2(100) NOT NULL,
-inq_phone VARCHAR2(300) NOT NULL,
-inq_cont VARCHAR2(4000) NOT NULL,
-inq_email VARCHAR2(500) NOT NULL,
-inq_file1 VARCHAR2(200),
+inq_phone VARCHAR2(300) NOT NULL, -- 휴대폰번호
+inq_cont VARCHAR2(4000) NOT NULL, -- 문의 내용
+inq_email VARCHAR2(500) NOT NULL, -- 이메일
+inq_file1 VARCHAR2(200),		  -- 파일
 inq_file2 VARCHAR2(200),
 inq_file3 VARCHAR2(200),
 inq_file4 VARCHAR2(200),
-inq_date DATE,
-mem_no NUMBER(38)
+inq_date DATE,					  -- 문의날짜
+mem_no NUMBER(38)				  -- 회원번호 참조컬럼
 );
 
 -- 시퀀스 생성
