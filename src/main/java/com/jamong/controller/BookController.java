6b767d544d34e80c7010141b2ed07373b9b7f8a3
gkrdud0822 <hakyoung0822@gmail.com>
2019-11-28 15:44:37 +0900

    페이지연동 이제 UI작업 해주세요

--- src/main/java/com/jamong/controller/BookController.java
+++ src/main/java/com/jamong/controller/BookController.java
@@ -2,6 +2,8 @@ package com.jamong.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.jamong.service.BookService;
 
@@ -10,4 +12,22 @@ public class BookController {
 
 	@Autowired
 	private BookService bookService;
+	
+	@RequestMapping("book_info")
+	public ModelAndView user_book_info() { // 책 정보
+		ModelAndView mv=new ModelAndView();
+		
+		mv.setViewName("jsp/book_info");
+		
+		return mv;
+	}
+	
+	@RequestMapping("new_book")
+	public ModelAndView user_new_book() { // 최신 책
+		ModelAndView mv=new ModelAndView();
+		
+		mv.setViewName("jsp/new_book");
+		
+		return mv;
+	}
 }
--- src/main/java/com/jamong/controller/OfferController.java
+++ src/main/java/com/jamong/controller/OfferController.java
@@ -10,4 +10,5 @@ public class OfferController {
 
 	@Autowired
 	private OfferService offerService;
+	
 }
