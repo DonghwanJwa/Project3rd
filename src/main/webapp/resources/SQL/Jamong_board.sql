4de4f2c0dcf162651bfd1fc21a508dd86736b597
gkrdud0822 <hakyoung0822@gmail.com>
2019-12-05 17:44:13 +0900

    보드 컨트롤러 만드는중 / 섬머노트 이미지 업로드 해야함

--- src/main/webapp/resources/SQL/Jamong_board.sql
+++ src/main/webapp/resources/SQL/Jamong_board.sql
@@ -2,13 +2,14 @@
 CREATE TABLE board(
 bo_no NUMBER(38) PRIMARY KEY, 		-- 게시판 번호 (seq)
 bo_title VARCHAR2(200) NOT NULL, 	-- 제목
-bo_subtitle VARCHAR2(100) NOT NULL, -- 소제목(부제)
-bo_cont CLOB NOT NULL, 				-- 내용  ※CLOB이 간혹 셀렉트에러가 있다고함
+bo_subtitle VARCHAR2(100),          -- 소제목(부제)
+bo_cont CLOB NOT NULL,    			-- 내용  ※CLOB이 간혹 셀렉트에러가 있다고함
 bo_thumbnail VARCHAR2(200),         -- 썸네일 이미지 or 색상
 bo_hit NUMBER(38) DEFAULT 0, 		-- 조회수
 bo_date DATE, 						-- 작성일
 bo_editdate DATE, 					-- 수정일
-bo_lock NUMBER(38) DEFAULT 0, 		-- 공개여부 / 비공개0 공개1
+bo_lock NUMBER(38),          		-- 공개여부 / 비공개 0, 공개 1
+bo_type NUMBER(38),                 -- 글타입 설정 / 칼럼 0, 에세이 1
 bo_like NUMBER(38) DEFAULT 0, 		-- 추천 (좋아요)
 book_order NUMBER(38), 				-- 책으로 묶었을때 순서
 fav_no NUMBER(38),					-- 카테고리 테이블 시퀀스 참조
@@ -23,7 +24,7 @@ INCREMENT BY 1
 MINVALUE 0
 NOCACHE;
 
--- 카테코리 테이블 참조컬럼 생성
+-- 카테고리 테이블 참조컬럼 생성
 ALTER TABLE board
 ADD CONSTRAINT bo_fav_no_fk FOREIGN KEY(fav_no)
 REFERENCES favorit(fav_no)
