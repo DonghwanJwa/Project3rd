5c1a7adf8df819c56e4c5eaeacecb4cf23d7151c
Haneul22 <dlth316@naver.com>
2019-12-10 17:47:51 +0900

    1.문의하기 관리자 페이지 뷰페이지 만드는중 2.문의하기 관리자 페이지 MVC 구축 작업중 3.문의하기 사용자 페이지 수정중

--- src/main/java/com/jamong/service/InquireService.java
+++ src/main/java/com/jamong/service/InquireService.java
@@ -1,9 +1,15 @@
 package com.jamong.service;
 
+import java.util.List;
+
 import com.jamong.domain.InquireVO;
 
 public interface InquireService {
 
 	void insertInquire(InquireVO i);
+
+	int getListCount(InquireVO i);
+
+	List<InquireVO> getInquireList(InquireVO i);
 
 }
