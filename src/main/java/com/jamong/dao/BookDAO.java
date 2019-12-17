ae0c43e3247121dd7779934e49fddc68d3f4a42e
NohZZang <shtlgus0915@gmail.com>
2019-12-17 17:47:37 +0900

    책 만들기 세션 아이디 값에 해당하는 board만 가져와서 목록 나오게 함

--- src/main/java/com/jamong/dao/BookDAO.java
+++ src/main/java/com/jamong/dao/BookDAO.java
@@ -3,9 +3,10 @@ package com.jamong.dao;
 import java.util.List;
 
 import com.jamong.domain.BoardVO;
+import com.jamong.domain.BookVO;
 
 public interface BookDAO {
 
-	List<BoardVO> getBList(BoardVO b);
+	List<BoardVO> getBList(String mem_id);
 
 }
--- src/main/java/com/jamong/service/BookServiceImpl.java
+++ src/main/java/com/jamong/service/BookServiceImpl.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.jamong.dao.BookDAO;
 import com.jamong.domain.BoardVO;
+import com.jamong.domain.BookVO;
 
 @Service
 public class BookServiceImpl implements BookService {
@@ -15,7 +17,8 @@ public class BookServiceImpl implements BookService {
 	private BookDAO bookDao;
 
 	@Override
-	public List<BoardVO> getBList(BoardVO b) {
-		return this.bookDao.getBList(b);
+	public List<BoardVO> getBList(String mem_id) {
+		return this.bookDao.getBList(mem_id);
 	}
+
 }
