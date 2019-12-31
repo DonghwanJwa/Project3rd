658a844f86fe585424cf22be34186478c5863f35
gkrdud0822 <gkrdud0822@naver.com>
2019-12-31 10:15:09 +0900

    게시글 등록 / 삭제시 멤버 테이블 게시글 카운트 업데이트, 팔로우(구독)도 구현, 카테고리 뷰페이지 추천작가탭에 게시글수, 구독자수 구현, 썸네일없는 글 언더바 색상 변경

--- src/main/java/com/jamong/service/SubscribeServiceImpl.java
+++ src/main/java/com/jamong/service/SubscribeServiceImpl.java
@@ -1,11 +1,12 @@
 package com.jamong.service;
 
 import java.util.HashMap;
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.jamong.dao.MemberDAO;
 import com.jamong.dao.SubscribeDAO;
 import com.jamong.domain.SubscribeVO;
 
@@ -14,15 +15,21 @@ public class SubscribeServiceImpl implements SubscribeService {
 
 	@Autowired
 	private SubscribeDAO subDao;
+	@Autowired
+	private MemberDAO memberDao;
 
+	@Transactional
 	@Override
 	public void insertSub(SubscribeVO subvo) {
 		this.subDao.insertSub(subvo);
+		this.memberDao.updateSubCount(subvo,1);
 	}
 
+	@Transactional
 	@Override
 	public void removeFollow(SubscribeVO subvo) {
 		this.subDao.deleteSub(subvo);
+		this.memberDao.updateSubCount(subvo,-1);
 	}
 	@Override
 	public int subCount(int mem_no) {
