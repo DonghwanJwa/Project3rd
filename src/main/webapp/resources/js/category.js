e6f593320fe8d5799b9f948ff742fbccc64ecd57
Haneul22 <dlth316@naver.com>
2020-01-03 15:59:59 +0900

    카테고리페이지 UI 및 기능 수정중

--- src/main/webapp/resources/js/category.js
+++ src/main/webapp/resources/js/category.js
@@ -5,17 +5,23 @@
 $(document).ready(function(){
 	
 	$(".cat_write_btn").click(function(){
-		$(".cat_write_btn").css("background-color","#FBFBFB");
-		$(".cat_book_btn").css("background-color","#FFDFBF");
+		$(".cat_write_btn").css("color","black");
+		$(".cat_book_btn").css("color","#ddd");
+		
+		$(".cat_write_btn").css("border","2px solid black");
+		$(".cat_book_btn").css("border","2px solid #bbb");
 		
 		$(".cat_writing_block").css("display","block");
 		$(".cat_book_block").css("display","none");
 	});
 	
 	$(".cat_book_btn").click(function(){
-		$(".cat_book_btn").css("background-color","#FBFBFB");
-		$(".cat_write_btn").css("background-color","#FFDFBF");
-	
+		$(".cat_book_btn").css("color","black");
+		$(".cat_write_btn").css("color","#ddd");
+		
+		$(".cat_book_btn").css("border","2px solid black");
+		$(".cat_write_btn").css("border","2px solid #bbb");
+		
 		$(".cat_writing_block").css("display","none");
 		$(".cat_book_block").css("display","flex");
 	});
